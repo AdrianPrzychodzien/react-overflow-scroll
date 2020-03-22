ab175bec00ba5f6cdac8c46c75ccfc356791f91d
Adrian Przychodzień <adrianprzychodzien@gmail.com>
2020-03-22 12:31:11 +0100

    withDots prop, dot change after button click and mouse grab

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
@@ -26,6 +34,26 @@ function _nonIterableRest() { throw new TypeError("Invalid attempt to destructur
 function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _templateObject9() {
+  var data = _taggedTemplateLiteral(["\n  margin-right: 10px;\n  &::after {\n    content: \"\";\n    display: block;\n    width: 15px;\n    height: 15px;\n    background: black;\n    border-radius: 50%;\n    border: 2px solid black\n  }\n"]);
+
+  _templateObject9 = function _templateObject9() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject8() {
+  var data = _taggedTemplateLiteral(["\n  display: flex;\n  justify-content: center;\n  color: black;\n   ", "\n"]);
+
+  _templateObject8 = function _templateObject8() {
+    return data;
+  };
+
+  return data;
+}
 
 function _templateObject7() {
   var data = _taggedTemplateLiteral(["\n  margin-bottom: 1rem;\n  position: relative;\n  display: flex;\n  justify-content: center;\n  :hover ", " {\n    opacity: 0.7;\n  }\n  :hover ", " {\n    opacity: 0.7;\n  }\n"]);
@@ -58,7 +86,7 @@ function _templateObject5() {
 }
 
 function _templateObject4() {
-  var data = _taggedTemplateLiteral(["\n  display: ", ";\n  margin-left: auto;\n"]);
+  var data = _taggedTemplateLiteral(["\n  display: ", ";\n  margin-left: auto;\n  padding: ", "\n"]);
 
   _templateObject4 = function _templateObject4() {
     return data;
@@ -68,7 +96,7 @@ function _templateObject4() {
 }
 
 function _templateObject3() {
-  var data = _taggedTemplateLiteral(["\n  display: ", ";\n  margin-right: auto;\n"]);
+  var data = _taggedTemplateLiteral(["\n  display: ", ";\n  margin-right: auto;\n  padding: ", "\n"]);
 
   _templateObject3 = function _templateObject3() {
     return data;
@@ -78,7 +106,7 @@ function _templateObject3() {
 }
 
 function _templateObject2() {
-  var data = _taggedTemplateLiteral(["\n  opacity: 0;\n  padding: 0.8rem 1.2rem;\n  border: 2px solid gray;\n  outline: none;\n  transform: translateY(-50%);\n  transition: all 0.2s linear;\n  :hover {\n    border-color: black;\n  }\n"]);
+  var data = _taggedTemplateLiteral(["\n  opacity: 0;\n  border: 2px solid gray;\n  outline: none;\n  transform: translateY(-50%);\n  transition: all 0.2s linear;\n  :hover {\n    border-color: black;\n  }\n"]);
 
   _templateObject2 = function _templateObject2() {
     return data;
@@ -106,33 +134,48 @@ var Button = _styledComponents["default"].button(_templateObject2());
 var ButtonLeft = (0, _styledComponents["default"])(Button)(_templateObject3(), function (_ref) {
   var canScrollLeft = _ref.canScrollLeft;
   return canScrollLeft ? "block" : "none";
+}, function (_ref2) {
+  var buttonSize = _ref2.buttonSize;
+  return buttonSize;
 });
-var ButtonRight = (0, _styledComponents["default"])(Button)(_templateObject4(), function (_ref2) {
-  var canScrollRight = _ref2.canScrollRight;
+var ButtonRight = (0, _styledComponents["default"])(Button)(_templateObject4(), function (_ref3) {
+  var canScrollRight = _ref3.canScrollRight;
   return canScrollRight ? "block" : "none";
+}, function (_ref4) {
+  var buttonSize = _ref4.buttonSize;
+  return buttonSize;
 });
 
 var Arrow = _styledComponents["default"].div(_templateObject5());
 
-var SliderContainer = _styledComponents["default"].div(_templateObject6(), function (_ref3) {
-  var withGrab = _ref3.withGrab;
+var SliderContainer = _styledComponents["default"].div(_templateObject6(), function (_ref5) {
+  var withGrab = _ref5.withGrab;
   return withGrab ? "grabbing" : "pointer";
-}, function (_ref4) {
-  var withGrab = _ref4.withGrab;
+}, function (_ref6) {
+  var withGrab = _ref6.withGrab;
   return withGrab ? "cursor: -webkit-grabbing" : "";
 });
 
 var StyledSlider = _styledComponents["default"].div(_templateObject7(), ButtonRight, ButtonLeft);
 
-var Slider = function Slider(_ref5) {
-  var data = _ref5.data,
-      _ref5$withArrows = _ref5.withArrows,
-      withArrows = _ref5$withArrows === void 0 ? true : _ref5$withArrows,
-      _ref5$withGrab = _ref5.withGrab,
-      withGrab = _ref5$withGrab === void 0 ? false : _ref5$withGrab,
-      _ref5$buttonSize = _ref5.buttonSize,
-      buttonSize = _ref5$buttonSize === void 0 ? '0.8rem 1.2rem' : _ref5$buttonSize,
-      scrollBy = _ref5.scrollBy;
+var DotsGroup = _styledComponents["default"].div(_templateObject8(), function (_ref7) {
+  var slide = _ref7.slide;
+  return slide ? "&:nth-child(2) div {\n      &:nth-child(".concat(slide, ") {\n      transition: all 0.3s linear;\n      transform: scale(1.5)\n    }\n  }") : null;
+});
+
+var Dot = _styledComponents["default"].div(_templateObject9());
+
+var Slider = function Slider(_ref8) {
+  var data = _ref8.data,
+      _ref8$withArrows = _ref8.withArrows,
+      withArrows = _ref8$withArrows === void 0 ? true : _ref8$withArrows,
+      _ref8$withGrab = _ref8.withGrab,
+      withGrab = _ref8$withGrab === void 0 ? false : _ref8$withGrab,
+      _ref8$buttonSize = _ref8.buttonSize,
+      buttonSize = _ref8$buttonSize === void 0 ? '0.8rem 1.2rem' : _ref8$buttonSize,
+      scrollBy = _ref8.scrollBy,
+      _ref8$withDots = _ref8.withDots,
+      withDots = _ref8$withDots === void 0 ? false : _ref8$withDots;
 
   var _useState = (0, _react.useState)(withArrows),
       _useState2 = _slicedToArray(_useState, 2),
@@ -154,6 +197,16 @@ var Slider = function Slider(_ref5) {
       canScrollRight = _useState8[0],
       setCanScrollRight = _useState8[1];
 
+  var _useState9 = (0, _react.useState)(),
+      _useState10 = _slicedToArray(_useState9, 2),
+      dots = _useState10[0],
+      setDots = _useState10[1];
+
+  var _useState11 = (0, _react.useState)(1),
+      _useState12 = _slicedToArray(_useState11, 2),
+      slide = _useState12[0],
+      setSlide = _useState12[1];
+
   var container = (0, _react.useRef)(null);
   (0, _react.useEffect)(function () {
     var slider = container.current;
@@ -165,20 +218,32 @@ var Slider = function Slider(_ref5) {
       debounceCheckForOverflow.cancel();
     };
   }, [data]);
+  (0, _react.useEffect)(function () {
+    var howManyDots = function howManyDots() {
+      var _container$current = container.current,
+          children = _container$current.children,
+          clientWidth = _container$current.clientWidth;
+      var fullItems = Math.floor(clientWidth / children[0].clientWidth);
+      var dotsNumber = Math.ceil(data.length / fullItems);
+      setDots(dotsNumber);
+    };
+
+    howManyDots();
+  }, []);
 
   var checkForScrollPosition = function checkForScrollPosition() {
-    var _container$current = container.current,
-        scrollLeft = _container$current.scrollLeft,
-        scrollWidth = _container$current.scrollWidth,
-        clientWidth = _container$current.clientWidth;
+    var _container$current2 = container.current,
+        scrollLeft = _container$current2.scrollLeft,
+        scrollWidth = _container$current2.scrollWidth,
+        clientWidth = _container$current2.clientWidth;
     setCanScrollLeft(scrollLeft > 0);
     setCanScrollRight(scrollLeft !== scrollWidth - clientWidth);
   };
 
   var checkForOverflow = function checkForOverflow() {
-    var _container$current2 = container.current,
-        scrollWidth = _container$current2.scrollWidth,
-        clientWidth = _container$current2.clientWidth;
+    var _container$current3 = container.current,
+        scrollWidth = _container$current3.scrollWidth,
+        clientWidth = _container$current3.clientWidth;
     var hasOverflow = scrollWidth > clientWidth;
     setHasOverflow(hasOverflow);
   };
@@ -187,16 +252,54 @@ var Slider = function Slider(_ref5) {
   var debounceCheckForScrollPosition = (0, _debounce["default"])(checkForScrollPosition, 200);
 
   var scrollContainerBy = function scrollContainerBy(distance) {
+    var _sizeOfFullItemsAndSi = sizeOfFullItemsAndSingleMargin(),
+        sizeOfFullItems = _sizeOfFullItemsAndSi.sizeOfFullItems,
+        singleChildrenMargin = _sizeOfFullItemsAndSi.singleChildrenMargin,
+        fullItems = _sizeOfFullItemsAndSi.fullItems;
+
+    var slider = container.current; // right button click
+
+    if (distance > 0) {
+      if (slider.scrollLeft <= distance - sizeOfFullItems) {
+        setSlide(2);
+      } else if (slider.scrollLeft <= distance) {
+        setSlide(3);
+      } else if (slider.scrollLeft <= distance * 2) {
+        setSlide(4);
+      } else if (slider.scrollLeft <= distance * 3) {
+        setSlide(5);
+      } else if (slider.scrollLeft <= distance * 4) {
+        setSlide(6);
+      } else if (slider.scrollLeft <= distance * 5) {
+        setSlide(7);
+      }
+    } // left button click
+
+
+    if (distance < 0) {
+      var num;
+
+      if (!canScrollRight) {
+        num = sizeOfFullItems - (slider.clientWidth - sizeOfFullItems) - singleChildrenMargin;
+      }
+
+      if (Math.abs(distance) === num) {
+        setSlide(slide - 1);
+      } else if (slider.scrollLeft) {
+        setSlide(slide - 1);
+      }
+    }
+
     container.current.scrollBy({
       left: distance,
       behavior: "smooth"
     });
   };
 
-  var scrollDistance = function scrollDistance(scrollBy) {
-    var _container$current3 = container.current,
-        children = _container$current3.children,
-        clientWidth = _container$current3.clientWidth;
+  var sizeOfFullItemsAndSingleMargin = function sizeOfFullItemsAndSingleMargin() {
+    var _container$current4 = container.current,
+        children = _container$current4.children,
+        clientWidth = _container$current4.clientWidth;
     var fullItems = Math.floor(clientWidth / children[0].clientWidth); // calculate margin of single element
 
     var nodeStyle = window.getComputedStyle(children[0]);
@@ -205,7 +308,24 @@ var Slider = function Slider(_ref5) {
     var marginRight = mr.substring(0, mr.length - 2);
     var marginLeft = ml.substring(0, ml.length - 2);
     var singleChildrenMargin = +marginRight + +marginLeft;
-    var sizeOfFullItems = fullItems * (children[0].clientWidth + singleChildrenMargin); // if there is scrollBy prop
+    var sizeOfFullItems = fullItems * (children[0].clientWidth + singleChildrenMargin);
+    return {
+      sizeOfFullItems: sizeOfFullItems,
+      singleChildrenMargin: singleChildrenMargin,
+      fullItems: fullItems
+    };
+  };
+
+  var scrollDistance = function scrollDistance(scrollBy) {
+    var _container$current5 = container.current,
+        children = _container$current5.children,
+        clientWidth = _container$current5.clientWidth;
+
+    var _sizeOfFullItemsAndSi2 = sizeOfFullItemsAndSingleMargin(),
+        sizeOfFullItems = _sizeOfFullItemsAndSi2.sizeOfFullItems,
+        singleChildrenMargin = _sizeOfFullItemsAndSi2.singleChildrenMargin,
+        fullItems = _sizeOfFullItemsAndSi2.fullItems; // if there is scrollBy prop
+
 
     if (scrollBy) {
       var userScrollBy = scrollBy * (children[0].clientWidth + singleChildrenMargin);
@@ -224,17 +344,42 @@ var Slider = function Slider(_ref5) {
     return sizeOfFullItems;
   };
 
+  var changeDotAfterWalk = function changeDotAfterWalk(distance) {
+    var _sizeOfFullItemsAndSi3 = sizeOfFullItemsAndSingleMargin(),
+        sizeOfFullItems = _sizeOfFullItemsAndSi3.sizeOfFullItems,
+        singleChildrenMargin = _sizeOfFullItemsAndSi3.singleChildrenMargin,
+        fullItems = _sizeOfFullItemsAndSi3.fullItems; // TODO: scrollBy prop
+    // TODO: disable button
+
+
+    if (distance === 0) {
+      setSlide(1);
+    } else if (distance <= sizeOfFullItems) {
+      setSlide(2);
+    } else if (distance <= sizeOfFullItems * 2) {
+      setSlide(3);
+    } else if (distance <= sizeOfFullItems * 3) {
+      setSlide(4);
+    } else if (distance <= sizeOfFullItems * 4) {
+      setSlide(5);
+    } else if (distance <= sizeOfFullItems * 5) {
+      setSlide(6);
+    } else if (distance <= sizeOfFullItems * 6) {
+      setSlide(7);
+    }
+  };
+
   var handleGrabbing = function handleGrabbing(e) {
-    var slider = document.querySelector('#slider');
+    var slider = container.current;
     var isDown = false;
     var startX;
-    var scrollLeftChange;
+    var scrollLeftLocal;
     slider.addEventListener('mousedown', function (e) {
       e.preventDefault();
       isDown = true;
       setArrows(false);
       startX = e.pageX - slider.offsetLeft;
-      scrollLeftChange = slider.scrollLeft;
+      scrollLeftLocal = slider.scrollLeft;
     });
     slider.addEventListener('mouseleave', function (e) {
       isDown = false;
@@ -245,13 +390,13 @@ var Slider = function Slider(_ref5) {
       setArrows(true);
     });
     slider.addEventListener('mousemove', function (e) {
-      if (isDown) {
-        e.preventDefault();
-        var x = e.pageX - slider.offsetLeft;
-        var walk = (x - startX) * 3; // scroll-fast
+      if (!isDown) return;
+      e.preventDefault();
+      var x = e.pageX - slider.offsetLeft;
+      var walk = (x - startX) * 3; // scroll-fast
 
-        slider.scrollLeft = scrollLeftChange - walk;
-      }
+      slider.scrollLeft = scrollLeftLocal - walk;
+      changeDotAfterWalk(slider.scrollLeft);
     });
   };
 
@@ -259,13 +404,12 @@ var Slider = function Slider(_ref5) {
     return direction === 'left' ? '<' : '>';
   };
 
-  return _react["default"].createElement(StyledSlider, null, _react["default"].createElement(SliderContainer, {
+  return _react["default"].createElement(_react["default"].Fragment, null, _react["default"].createElement(StyledSlider, null, _react["default"].createElement(SliderContainer, {
     withGrab: withGrab,
     onClick: withGrab ? function (e) {
       return handleGrabbing(e);
     } : null,
-    ref: container,
-    id: "slider"
+    ref: container
   }, data), arrows && _react["default"].createElement(ButtonGroup, null, _react["default"].createElement(ButtonLeft, {
     buttonSize: buttonSize,
     canScrollLeft: canScrollLeft,
@@ -278,7 +422,13 @@ var Slider = function Slider(_ref5) {
     onClick: function onClick() {
       return scrollContainerBy(scrollDistance(scrollBy));
     }
-  }, _react["default"].createElement(Arrow, null, returnArrow('right')))));
+  }, _react["default"].createElement(Arrow, null, returnArrow('right'))))), withDots && _react["default"].createElement(DotsGroup, {
+    slide: slide
+  }, _toConsumableArray(Array(dots).keys()).map(function (dot) {
+    return _react["default"].createElement(Dot, {
+      key: dot
+    });
+  })));
 };
 
 var _default = Slider;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const ButtonGroup = styled.div`
 `;
 const Button = styled.button`
   opacity: 0;
-  padding: 0.8rem 1.2rem;
   border: 2px solid gray;
   outline: none;
   transform: translateY(-50%);
@@ -22,16 +21,17 @@ const Button = styled.button`
 const ButtonLeft = styled(Button)`
   display: ${({ canScrollLeft }) => (canScrollLeft ? `block` : `none`)};
   margin-right: auto;
+  padding: ${({ buttonSize }) => buttonSize}
 `;
 const ButtonRight = styled(Button)`
   display: ${({ canScrollRight }) => (canScrollRight ? `block` : `none`)};
   margin-left: auto;
+  padding: ${({ buttonSize }) => buttonSize}
 `;
 const Arrow = styled.div`
   font-weight: 600;
   transform: scale(1.5, 2)
 `
-
 const SliderContainer = styled.div`
   ::-webkit-scrollbar {
     width: 0px;
@@ -42,8 +42,7 @@ const SliderContainer = styled.div`
   overflow: auto;
   cursor: ${({ withGrab }) => withGrab ? `grabbing` : `pointer`};
   ${({ withGrab }) => withGrab ? `cursor: -webkit-grabbing` : ``};
-`;
-
+`
 const StyledSlider = styled.div`
   margin-bottom: 1rem;
   position: relative;
@@ -55,13 +54,41 @@ const StyledSlider = styled.div`
   :hover ${ButtonLeft} {
     opacity: 0.7;
   }
-`;
+`
+const DotsGroup = styled.div`
+  display: flex;
+  justify-content: center;
+  color: black;
+   ${({ slide }) => slide ?
+    `&:nth-child(2) div {
+      &:nth-child(${slide}) {
+      transition: all 0.3s linear;
+      transform: scale(1.5)
+    }
+  }` : null
+  }
+`
+const Dot = styled.div`
+  margin-right: 10px;
+  &::after {
+    content: "";
+    display: block;
+    width: 15px;
+    height: 15px;
+    background: black;
+    border-radius: 50%;
+    border: 2px solid black
+  }
+`
 
-const Slider = ({ data, withArrows = true, withGrab = false, buttonSize = '0.8rem 1.2rem', scrollBy }) => {
+
+const Slider = ({ data, withArrows = true, withGrab = false, buttonSize = '0.8rem 1.2rem', scrollBy, withDots = false }) => {
   const [arrows, setArrows] = useState(withArrows)
   const [hasOverflow, setHasOverflow] = useState(false);
   const [canScrollLeft, setCanScrollLeft] = useState(false);
   const [canScrollRight, setCanScrollRight] = useState(false);
+  const [dots, setDots] = useState();
+  const [slide, setSlide] = useState(1)
 
   const container = useRef(null);
 
@@ -84,6 +111,18 @@ const Slider = ({ data, withArrows = true, withGrab = false, buttonSize = '0.8re
     };
   }, [data]);
 
+  useEffect(() => {
+    const howManyDots = () => {
+      const { children, clientWidth } = container.current;
+      let fullItems = Math.floor(clientWidth / children[0].clientWidth);
+
+      const dotsNumber = Math.ceil(data.length / fullItems);
+      setDots(dotsNumber)
+    }
+
+    howManyDots()
+  }, [])
+
   const checkForScrollPosition = () => {
     const { scrollLeft, scrollWidth, clientWidth } = container.current;
     setCanScrollLeft(scrollLeft > 0);
@@ -100,10 +139,44 @@ const Slider = ({ data, withArrows = true, withGrab = false, buttonSize = '0.8re
   const debounceCheckForScrollPosition = debounce(checkForScrollPosition, 200);
 
   const scrollContainerBy = distance => {
+    const { sizeOfFullItems, singleChildrenMargin, fullItems } = sizeOfFullItemsAndSingleMargin()
+
+    let slider = container.current
+
+    // right button click
+    if (distance > 0) {
+      if (slider.scrollLeft <= distance - sizeOfFullItems) {
+        setSlide(2)
+      } else if (slider.scrollLeft <= distance) {
+        setSlide(3)
+      } else if (slider.scrollLeft <= distance * 2) {
+        setSlide(4)
+      } else if (slider.scrollLeft <= distance * 3) {
+        setSlide(5)
+      } else if (slider.scrollLeft <= distance * 4) {
+        setSlide(6)
+      } else if (slider.scrollLeft <= distance * 5) {
+        setSlide(7)
+      }
+    }
+
+    // left button click
+    if (distance < 0) {
+      let num
+      if (!canScrollRight) {
+        num = sizeOfFullItems - (slider.clientWidth - sizeOfFullItems) - singleChildrenMargin;
+      }
+      if (Math.abs(distance) === num) {
+        setSlide(slide - 1)
+      } else if (slider.scrollLeft) {
+        setSlide(slide - 1)
+      }
+    }
+
     container.current.scrollBy({ left: distance, behavior: "smooth" });
   };
 
-  const scrollDistance = (scrollBy) => {
+  const sizeOfFullItemsAndSingleMargin = () => {
     const { children, clientWidth } = container.current;
     let fullItems = Math.floor(clientWidth / children[0].clientWidth);
 
@@ -119,6 +192,13 @@ const Slider = ({ data, withArrows = true, withGrab = false, buttonSize = '0.8re
     let sizeOfFullItems =
       fullItems * (children[0].clientWidth + singleChildrenMargin);
 
+    return { sizeOfFullItems, singleChildrenMargin, fullItems }
+  }
+
+  const scrollDistance = (scrollBy) => {
+    const { children, clientWidth } = container.current;
+    const { sizeOfFullItems, singleChildrenMargin, fullItems } = sizeOfFullItemsAndSingleMargin()
+
     // if there is scrollBy prop
     if (scrollBy) {
       let userScrollBy = scrollBy * (children[0].clientWidth + singleChildrenMargin)
@@ -130,27 +210,47 @@ const Slider = ({ data, withArrows = true, withGrab = false, buttonSize = '0.8re
       return userScrollBy
     }
 
-
     if (!canScrollRight) {
-      return sizeOfFullItems - (clientWidth - sizeOfFullItems) -
-        singleChildrenMargin;
+      return sizeOfFullItems - (clientWidth - sizeOfFullItems) - singleChildrenMargin;
     }
 
     return sizeOfFullItems;
   };
 
+  const changeDotAfterWalk = distance => {
+    const { sizeOfFullItems, singleChildrenMargin, fullItems } = sizeOfFullItemsAndSingleMargin()
+
+    // TODO: scrollBy prop
+    // TODO: disable button
+    if (distance === 0) {
+      setSlide(1)
+    } else if (distance <= sizeOfFullItems) {
+      setSlide(2)
+    } else if (distance <= sizeOfFullItems * 2) {
+      setSlide(3)
+    } else if (distance <= sizeOfFullItems * 3) {
+      setSlide(4)
+    } else if (distance <= sizeOfFullItems * 4) {
+      setSlide(5)
+    } else if (distance <= sizeOfFullItems * 5) {
+      setSlide(6)
+    } else if (distance <= sizeOfFullItems * 6) {
+      setSlide(7)
+    }
+  }
+
   const handleGrabbing = e => {
-    const slider = document.querySelector('#slider')
+    let slider = container.current
     let isDown = false;
     let startX;
-    let scrollLeftChange;
+    let scrollLeftLocal;
 
     slider.addEventListener('mousedown', e => {
       e.preventDefault()
       isDown = true
       setArrows(false)
       startX = e.pageX - slider.offsetLeft
-      scrollLeftChange = slider.scrollLeft
+      scrollLeftLocal = slider.scrollLeft
     })
     slider.addEventListener('mouseleave', e => {
       isDown = false
@@ -161,46 +261,55 @@ const Slider = ({ data, withArrows = true, withGrab = false, buttonSize = '0.8re
       setArrows(true)
     })
     slider.addEventListener('mousemove', e => {
-      if (isDown) {
-        e.preventDefault()
-        const x = e.pageX - slider.offsetLeft
-        const walk = (x - startX) * 3 // scroll-fast
-        slider.scrollLeft = scrollLeftChange - walk
-      }
+      if (!isDown) return
+      e.preventDefault()
+      const x = e.pageX - slider.offsetLeft
+      const walk = (x - startX) * 3 // scroll-fast
+      slider.scrollLeft = scrollLeftLocal - walk
+
+      changeDotAfterWalk(slider.scrollLeft)
     })
   }
 
   const returnArrow = direction => direction === 'left' ? '<' : '>'
 
   return (
-    <StyledSlider>
-      <SliderContainer
-        withGrab={withGrab}
-        onClick={withGrab ? (e => handleGrabbing(e)) : null}
-        ref={container}
-        id="slider"
-      >
-        {data}
-      </SliderContainer>
-      {arrows && (
-        <ButtonGroup>
-          <ButtonLeft
-            buttonSize={buttonSize}
-            canScrollLeft={canScrollLeft}
-            onClick={() => scrollContainerBy(-scrollDistance(scrollBy))}
-          >
-            <Arrow>{returnArrow('left')}</Arrow>
-          </ButtonLeft>
-          <ButtonRight
-            buttonSize={buttonSize}
-            canScrollRight={canScrollRight}
-            onClick={() => scrollContainerBy(scrollDistance(scrollBy))}
-          >
-            <Arrow>{returnArrow('right')}</Arrow>
-          </ButtonRight>
-        </ButtonGroup>
+    <>
+      <StyledSlider>
+        <SliderContainer
+          withGrab={withGrab}
+          onClick={withGrab ? (e => handleGrabbing(e)) : null}
+          ref={container}
+        >
+          {data}
+        </SliderContainer>
+        {arrows && (
+          <ButtonGroup >
+            <ButtonLeft
+              buttonSize={buttonSize}
+              canScrollLeft={canScrollLeft}
+              onClick={() => scrollContainerBy(-scrollDistance(scrollBy))}
+            >
+              <Arrow>{returnArrow('left')}</Arrow>
+            </ButtonLeft>
+            <ButtonRight
+              buttonSize={buttonSize}
+              canScrollRight={canScrollRight}
+              onClick={() => scrollContainerBy(scrollDistance(scrollBy))}
+            >
+              <Arrow>{returnArrow('right')}</Arrow>
+            </ButtonRight>
+          </ButtonGroup>
+        )}
+      </StyledSlider>
+      {withDots && (
+        <DotsGroup slide={slide} >
+          {[...Array(dots).keys()].map(dot => {
+            return <Dot key={dot}></Dot>
+          })}
+        </DotsGroup>
       )}
-    </StyledSlider>
+    </>
   );
 };
 
